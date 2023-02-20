bba40bbbb80f4c692cd7e81e14c377814de8950f
Martin Dreyer <martinchristiandreyer@gmail.com>
2023-02-20 10:42:05 +0100

    recognize new pnr's in data

--- Scraper.py
+++ Scraper.py
@@ -29,10 +29,10 @@ def check_at_smiley(lower, upper):
     # Hvis status er OK parser scriptet indholdet, henter formData og tilføjer det til et dictionary
     if r.status_code == 200:
         soup = BeautifulSoup(r.content, "html.parser")
-        
+
         viewstate = soup.select("#__VIEWSTATE")[0]['value']
         eventvalidation = soup.select("#__EVENTVALIDATION")[0]['value']
-           
+
         formData = {
             '__EVENTVALIDATION': eventvalidation,
             '__VIEWSTATE': viewstate,
@@ -45,17 +45,15 @@ def check_at_smiley(lower, upper):
             'ddlSmiley':0,
             'btnDownload': 'Download smileydata'   
         }
-        
-        
+
+
     # Send POST-request, hvor den netop hentede formdata sendes med
     try:
         r = requests.post(url, data=formData, headers=header)
         r.raise_for_status()
     except requests.exceptions.HTTPError as error:
         errors.append(error)
-
     try:
-
         # Konverter responsdata til en string og indlæs i en DataFrame
         # filtrer datasæt efter fynske virksomheder og røde smileyer
         text = r.text
@@ -72,6 +70,7 @@ def check_at_smiley(lower, upper):
         new["id"] = new[list(new.columns)].astype(str).sum(axis=1).map(hash)
         new = new.reset_index(drop=True)
         new_smiley_ids = list(dict.fromkeys(list(new["id"])))
+    
     except:
         print("There was an error adding IDs to new dataset")
 
@@ -88,16 +87,21 @@ def check_at_smiley(lower, upper):
         old["id"] = old[list(old.columns)].astype(str).sum(axis=1).map(hash)
         old_smiley_ids = list(dict.fromkeys(list(old["id"])))
         old = old.reset_index(drop=True)
-        
+
     except:
         old = pd.DataFrame(list())
         old_smiley_ids = list()
-    
+
     try:
         # Opret tomme dataframes, hvor vi kan lægge henholdsvis tilføjede og fjernede smileyer
         added = pd.DataFrame()
         removed = pd.DataFrame()
+        added_pnr = pd.DataFrame()
+        removed_pnr = pd.DataFrame()
+        
+
         changes_found = False
+        
 
         # Loop igennem nye smileyer for at se, om der eksisterer noget i de nye, som ikke eksisterede i det gamle
         # Hvis der gør, betyder det, at der er tilføjet en smiley
@@ -115,13 +119,45 @@ def check_at_smiley(lower, upper):
                 new_row = old[old["id"] == hit]
                 removed = pd.concat([new_row, removed])
                 changes_found = True
-
+        
+        
+        # Lav liste over p-numre i henholdsvis gammelt og nyt datasæt
+        # Sammenhold dem - hvis der er tilføjede eller fjernede p-numre
+        # samles de i et csv.   
+        added_pnr_list = list()
+        removed_pnr_list = list()
+        
+        if len(added) > 0:
+            added_pnr_list = list(dict.fromkeys(list(added["PNR"])))
+        if len(removed)>0:
+            removed_pnr_list = list(dict.fromkeys(list(removed["PNR"])))
+        
+        for hit in added_pnr_list:
+            if not hit in removed_pnr_list:
+                new_row = added[added["PNR"] == hit]
+                added_pnr = pd.concat([new_row, added_pnr])
+                changes_found = True
+                
+        for hit in removed_pnr_list:
+            if not hit in added_pnr_list:
+                new_row = removed[removed["PNR"] == hit]
+                removed_pnr = pd.concat([new_row, removed_pnr])
+                changes_found = True
+                
         if changes_found is True:
             df.to_csv("csv/at.csv",sep=";",index=False)
-            
+
         added.to_csv("csv/added_at.csv",sep=";",index=False)
         removed.to_csv("csv/removed_at.csv",sep=";",index=False)
+        
+        added_pnr.to_csv("csv/added_pnr.csv", sep=";", index=False)
+        removed_pnr.to_csv("csv/removed_pnr.csv", sep=";",index=False)
+                        
+
+
     except:
         print("There was an error comparing new and old dataset")
 
+
+# Kald funktionen
 check_at_smiley(4999,6000)
